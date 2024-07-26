2e68c22395333dc5c2fd99afbd1e04ed4482a12d
aynurdavud92 <aynurdavud92@gmail.com>
2024-07-26 07:24:30 +0300

    first commit

---
+++ js/homesliders.js
@@ -0,0 +1,52 @@
+$('.slick-products').slick({
+    dots:true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+     
+      $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
+            }
+          }
+        ]
+      });
---
+++ js/onboarding.js
@@ -0,0 +1,4 @@
+$('.slick-products').slick({
+    dots: true,
+    nextArrow: '<button id="next" class="next_button">->',
+ });
